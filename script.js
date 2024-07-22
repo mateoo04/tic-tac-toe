5609006d1e3a5bf585d65251f8046e42bd6f61c7
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-07-22 16:07:15 +0200

    Add files and factory functions to script.js
    Added Gameboard, Player and GameController
    factory functions to script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+function Gameboard() {
+    let board = []
+
+    //board creation
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i].push(0);
+        }
+    }
+
+    const printBoard = () => {
+        console.log(board);
+    };
+
+    const setValue = (row, column, player) => {
+        board[row][colum] = value;
+    };
+
+    return { printBoard, setValue }
+}
+
+function Player() {
+
+    const players = [
+        { name: 'Player 1', token: 1 },
+        { name: 'Player 2', token: 2 }
+    ];
+
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => { return activePlayer; };
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    return { getActivePlayer, switchPlayer }
+}
+
+function GameController() {
+
+    let board = Gameboard();
+    let player = Player();
+
+    board.printBoard();
+
+    const playRound = () => {
+        let rowOfMove = prompt(`${player.getActivePlayer().name}, please type in the row in which you want to make your move:`);
+        let colOfMove = prompt(`${player.getActivePlayer().name}, please type in the column in which you want to make your move:`);
+    };
+
+    playRound();
+
+}
+
+GameController();
